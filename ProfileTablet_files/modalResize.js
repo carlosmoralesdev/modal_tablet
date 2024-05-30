c96dc8465190d20a05af3e07f96b131ac0a29b82
Morales <Carlos.Morales@seguroscaracas.com>
2024-05-30 18:38:42 -0400

    change

--- ProfileTablet_files/modalResize.js
+++ ProfileTablet_files/modalResize.js
@@ -1,47 +1,53 @@
 function changeHeight() {
+
     let valorHeight = window.innerHeight
     let array = []
     let nuevoValor = 0;
 
-    array = document.getElementsByClassName('modal-body')
-    for (let index = 0; index < array.length; index++) {
-        let elemento = array[index]
-        //elemento.classList.add("max-h100vh")
-        nuevoValor = valorHeight - ((valorHeight * 0.03 * 2) + (50 * 2) + 60)
-        elemento.style.setProperty("height", "50px", "important")
-        elemento.style.height = nuevoValor + "px"
+    if (navigator.userAgent.toUpperCase().includes("IPAD")
+        || navigator.userAgent.toUpperCase().includes("IPHONE")
+        || navigator.userAgent.toUpperCase().includes("TABLET")
+        || navigator.userAgent.toUpperCase().includes("PHONE")
+        || navigator.userAgent.toUpperCase().includes("ANDROID")) {
+
+
+        array = document.getElementsByClassName('modal-body')
+        for (let index = 0; index < array.length; index++) {
+            let elemento = array[index]
+            nuevoValor = valorHeight - ((valorHeight * 0.03 * 2) + (50 * 2) + 60)
+            elemento.style.height = nuevoValor + "px"
+        }
+
+        //alert("Body: " + nuevoValor + "px, content: " + valorHeight)
+
+        //modal content
+        if (navigator.userAgent.toUpperCase().includes("IPAD")
+            || navigator.userAgent.toUpperCase().includes("TABLET")) {
+            array = document.getElementsByClassName('modal-content')
+            for (let index = 0; index < array.length; index++) {
+                let elemento = array[index]
+                elemento.style.marginTop = (valorHeight * -0.05) + "px"
+                elemento.style.marginBottom = (valorHeight * 0.03) + "px"
+            }
+        }
+
+        //modal-header
+        array = document.getElementsByClassName('modal-header')
+        for (let index = 0; index < array.length; index++) {
+            let elemento = array[index]
+            elemento.style.setProperty("height", "50px", "important")
+        }
+
+        //modal-footer
+        array = document.getElementsByClassName('modal-footer')
+        for (let index = 0; index < array.length; index++) {
+            let elemento = array[index]
+            elemento.style.setProperty("height", "50px", "important")
+        }
+
     }
 
-    alert("Body: " + nuevoValor + "px, content: " + valorHeight)
 
-    //modal content
-    array = document.getElementsByClassName('modal-content')
-    for (let index = 0; index < array.length; index++) {
-        let elemento = array[index]
-        elemento.style.marginTop = (valorHeight * 0.03) + "px"
-        elemento.style.marginBottom = (valorHeight * 0.03) + "px"
-    }
-
-    //modal-header
-    array = document.getElementsByClassName('modal-header')
-    for (let index = 0; index < array.length; index++) {
-        let elemento = array[index]
-        elemento.style.setProperty("height", "50px", "important")
-    }
-
-    //modal-footer
-    array = document.getElementsByClassName('modal-footer')
-    for (let index = 0; index < array.length; index++) {
-        let elemento = array[index]
-        elemento.style.setProperty("height", "50px", "important")
-    }
-
-    try{
-        let valor = document.querySelector('.navigation-bar').clientHeight
-        alert("Altura de la barra de navegacion del mobile: " + valor)
-    }catch(e){
-        alert(e) 
-    }
 
 }
 
