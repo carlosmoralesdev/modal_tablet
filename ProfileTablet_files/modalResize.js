1121bd50be17c7d2cb7105a7133306bf2feec57d
Morales <Carlos.Morales@seguroscaracas.com>
2024-05-30 18:07:30 -0400

    comA

--- ProfileTablet_files/modalResize.js
+++ ProfileTablet_files/modalResize.js
@@ -1,5 +1,3 @@
-let resto = 100;
-
 function changeHeight() {
     let valorHeight = window.innerHeight
     let array = []
@@ -9,7 +7,7 @@ function changeHeight() {
     for (let index = 0; index < array.length; index++) {
         let elemento = array[index]
         //elemento.classList.add("max-h100vh")
-        nuevoValor = valorHeight - ((valorHeight * 0.03 * 2) + (50 * 2) + 40)
+        nuevoValor = valorHeight - ((valorHeight * 0.03 * 2) + (50 * 2) + 200)
         elemento.style.setProperty("height", "50px", "important")
         elemento.style.height = nuevoValor + "px"
     }
@@ -42,5 +40,5 @@ function changeHeight() {
 
 window.addEventListener("resize", changeHeight);
 //document.addEventListener("DOMContentLoaded", changeHeight);
-document.addEventListener("load", changeHeight);
+document.addEventListener("DOMContentLoaded", changeHeight);
 changeHeight()
