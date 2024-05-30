7d8eabf790c052dabba0840191aa168ae9887f5d
Morales <Carlos.Morales@seguroscaracas.com>
2024-05-30 17:25:55 -0400

    ajuste

--- ProfileTablet_files/modalResize.js
+++ ProfileTablet_files/modalResize.js
@@ -5,8 +5,11 @@ function changeHeight() {
     array = document.getElementsByClassName('modal-body')
     for (let index = 0; index < array.length; index++) {
         let elemento = array[index]
-        elemento.classList.add("max-h100vh")
-        elemento.style.height = (valorHeight - ((valorHeight * 0.03 * 2) + (50 * 2)) + 100) + "px"
+        //elemento.classList.add("max-h100vh")
+        let nuevoValor = valorHeight - ((valorHeight * 0.03 * 2) + (50 * 2)) + 100
+        alert("Nuevo valor: " + nuevoValor + "px")
+        elemento.style.setProperty("height", "50px", "important")
+        elemento.style.height = nuevoValor + "px"
     }
 
     //modal content
@@ -21,14 +24,14 @@ function changeHeight() {
     array = document.getElementsByClassName('modal-header')
     for (let index = 0; index < array.length; index++) {
         let elemento = array[index]
-        elemento.classList.add("height-50-important")
+        elemento.style.setProperty("height", "50px", "important")
     }
 
     //modal-footer
     array = document.getElementsByClassName('modal-footer')
     for (let index = 0; index < array.length; index++) {
         let elemento = array[index]
-        elemento.classList.add("height-50-important")
+        elemento.style.setProperty("height", "50px", "important")
     }
 
 }
